4bb6143ec0de1137f7dae2b7666dd20ded7f2ec9
林海瑞 <hairui.lin@hekr.me>
2018-08-31 10:56:47 +0800

    debug 增加创建人群选择人群属性的时候防抖动操作

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
   rules: {
     // don't require .vue extension when importing
     'arrow-parens': 0,
+    'func-names': 0,
     'import/extensions': ['error', 'always', {
       js: 'never',
       vue: 'never'
