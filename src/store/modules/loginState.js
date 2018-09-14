847f9c439338466d80798756b0865ac00cbe21c8
林海瑞 <hairui.lin@hekr.me>
2018-09-14 13:36:57 +0800

    pull

--- src/store/modules/loginState.js
+++ src/store/modules/loginState.js
@@ -9,7 +9,7 @@ const id = {
 const state = {
   isLogin: false,
   bindTaobaoName: '',
-  editorExtensionId: id.kzp, // 插件ID
+  editorExtensionId: id.test, // 插件ID
 };
 // mutations
 const mutations = {
