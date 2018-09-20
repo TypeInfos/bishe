2d8cb0a0d05b0aa064a6a43b640601af41b9f80a
TypeInfos <1443970739@qq.com>
2018-09-20 20:16:33 +0800

    上线

--- src/store/modules/loginState.js
+++ src/store/modules/loginState.js
@@ -10,7 +10,7 @@ const id = {
 const state = {
   isLogin: false,
   bindTaobaoName: '',
-  editorExtensionId: id.test, // 插件ID
+  editorExtensionId: id.online, // 插件ID
 };
 // mutations
 const mutations = {
